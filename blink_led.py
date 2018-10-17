040a8808abad98a929c5609eeda24f6772526ffc
Zhifei Yang <zhifei.yang@arm.com>
2018-10-17 16:14:18 +0800

    Add example GPIO code for SNP410C board
    Signed-off-by: Zhifei Yang <zhifei.yang@arm.com>

---
+++ blink_led.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+
+from GPIOLibrary import GPIOProcessor
+import time
+
+GP = GPIOProcessor()
+
+try:
+    Pin27 = GP.getPin27()
+    Pin27.out()
+
+    Pin29 = GP.getPin29()
+    Pin29.input()	
+
+    for i in range(0,20):
+        pinValue = Pin29.getValue();
+	
+	if pinValue == 1:
+	    Pin27.high()
+        else:
+            Pin27.low()
+        time.sleep(1)
+
+finally:
+    GP.cleanup()
